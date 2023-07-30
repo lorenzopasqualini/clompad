160fe178d0f45fd5bba0bd626ed698c79c8a79b1
Lorenzo Pasqualini <lorenzopasqualini@outlook.com>
2023-07-31 00:20:24 +0200

    v1.0

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -21,6 +21,11 @@ export function MainNav({
 			active: pathname === `/${params.storeId}/billboards`,
 		},
 		{
+			href: `/${params.storeId}/categories`,
+			label: 'Categories',
+			active: pathname === `/${params.storeId}/categories`,
+		},
+		{
 			href: `/${params.storeId}/settings`,
 			label: 'Settings',
 			active: pathname === `/${params.storeId}/settings`,
@@ -28,18 +33,19 @@ export function MainNav({
 	];
 	return (
 		<nav
-			className={(cn('flex items-center space-x-4 lg:space-x-6'), className)}
+			className={cn('flex items-center space-x-4 lg:space-x-6', className)}
+			{...props}
 		>
 			{routes.map((route) => (
 				<Link
 					key={route.href}
 					href={route.href}
-					className={
-						(cn('text-sm font-medium transition-colors hover:text-primary'),
+					className={cn(
+						'text-sm font-medium transition-colors hover:text-primary',
 						route.active
 							? 'text-black dark:text-white'
-							: 'text-muted-foreground')
-					}
+							: 'text-muted-foreground'
+					)}
 				>
 					{route.label}
 				</Link>
