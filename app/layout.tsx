dcc2734396fa668617a618bd166cfa450d6900a2
Lorenzo Pasqualini <lorenzopasqualini@outlook.com>
2023-08-06 14:34:47 +0200

    v1.7
    Dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
-import { ModalProvider } from '@/providers/modal-provider';
-import { ToasterProvider } from '@/providers/toast-providers';
+import { ThemeProvider } from '@/providers/themeProvider';
+import { ToasterProvider } from '@/providers/toastProvider';
+import { ModalProvider } from '@/providers/modalProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
+					<ThemeProvider attribute="class" defaultTheme="system" enableSystem />
 					<ToasterProvider />
 					<ModalProvider />
 					{children}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { MainNav } from '@/components/main-nav';
 import StoreSwitcher from '@/components/store-switcher';
 import prismadb from '@/lib/prismadb';
 import { redirect } from 'next/navigation';
+import { ModeToggle } from './theme';
 
 const Navbar = async () => {
 	const { userId } = auth();
@@ -22,6 +23,7 @@ const Navbar = async () => {
 				<StoreSwitcher items={stores} />
 				<MainNav className="mx-6" />
 				<div className="ml-auto flex items-center space-x-4">
+					<ModeToggle />
 					<UserButton afterSignOutUrl="/" />
 				</div>
 			</div>
